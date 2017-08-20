02af2dc038998b3c38487c7794c5f94150cc0251
Andreas Rammhold <andreas@rammhold.de>
2017-08-20 18:40:40 +0200

    Allow talk lookup via talk PK

--- src/pretalx/agenda/urls.py
+++ src/pretalx/agenda/urls.py
@@ -11,5 +11,6 @@ agenda_urls = [
 
     url('^(?P<event>\w+)/location/$', location.LocationView.as_view(), name='location'),
     url('^(?P<event>\w+)/talk/(?P<slug>\w+)/$', schedule.TalkView.as_view(), name='talk'),
+    url('^(?P<event>\w+)/talk/(?P<pk>\d+)/$', schedule.TalkView.as_view(), name='talk'),
     url('^(?P<event>\w+)/speaker/(?P<name>\w+)/$', speaker.SpeakerView.as_view(), name='speaker'),
 ]
