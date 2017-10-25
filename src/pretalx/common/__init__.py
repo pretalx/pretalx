a3741af880edaae08e7b50daec47e80dd34fd929
luto <m@luto.at>
2017-10-25 15:44:08 +0200

    [common] regenerate custom css & color on startup, fix #188

--- src/pretalx/common/__init__.py
+++ src/pretalx/common/__init__.py
@@ -6,6 +6,13 @@ from django.apps import AppConfig
 class CommonConfig(AppConfig):
     name = 'pretalx.common'
 
+    def ready(self):
+        from pretalx.event.models import Event
+        from pretalx.common.tasks import regenerate_css
+
+        for event in Event.objects.all():
+            regenerate_css.apply_async(args=(event.pk,))
+
 
 with suppress(ImportError):
     import pretalx.celery_app as celery  # NOQA
