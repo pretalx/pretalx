1ad85eca08db6778abb78f2a0c3dfa6b568e7fcf
luto <m@luto.at>
2017-10-25 16:51:44 +0200

    [common] fix crash in compilemessages etc., if there is no DB

--- src/pretalx/common/__init__.py
+++ src/pretalx/common/__init__.py
@@ -9,6 +9,13 @@ class CommonConfig(AppConfig):
     def ready(self):
         from pretalx.event.models import Event
         from pretalx.common.tasks import regenerate_css
+        from django.db import connection
+
+        if Event._meta.db_table not in connection.introspection.table_names():
+            # commands like `compilemessages` execute ready(), but do not
+            # require a database to be present. Bail out early, if the Event
+            # table has not been created yet.
+            return
 
         for event in Event.objects.all():
             regenerate_css.apply_async(args=(event.pk,))
