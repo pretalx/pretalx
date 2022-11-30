3249347825590595ed941947e49b4784dfa27564
Antoine Mottier <antoine.mottier83+github@gmail.com>
2022-11-30 12:10:23 +0100

    Fix additional speaker help text
    CfP form allow to add a single co-speaker email address so replace "their email address" by "his email address" and "invite them" by "invite him".

--- src/pretalx/submission/forms/submission.py
+++ src/pretalx/submission/forms/submission.py
@@ -17,7 +17,7 @@ class InfoForm(CfPFormMixin, RequestRequire, PublicContent, forms.ModelForm):
     additional_speaker = forms.EmailField(
         label=_("Additional Speaker"),
         help_text=_(
-            "If you have a co-speaker, please add their email address here, and we will invite them to create an account. If you have more than one co-speaker, you can add more speakers after finishing the proposal process."
+            "If you have a co-speaker, please add his email address here, and we will invite him to create an account. If you have more than one co-speaker, you can add more speakers after finishing the proposal process."
         ),
         required=False,
     )
