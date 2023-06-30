724bd034acc9570f19ab9bc7d889e5cf2350c025
Franziska Kunsmann <fkunsmann@seibert-media.net>
2023-06-30 11:17:46 +0200

    rework gravatar caching so caching happens immediately after saving the form

--- src/pretalx/person/tasks.py
+++ src/pretalx/person/tasks.py
@@ -11,31 +11,30 @@ logger = logging.getLogger(__name__)
 
 
 @app.task()
-def gravatar_cache(*args, **kwargs):
-    users_with_gravatar = User.objects.filter(get_gravatar=True)
+def gravatar_cache(person_id: int):
+    user = User.objects.filter(pk=person_id).first()
 
-    for user in users_with_gravatar:
-        r = get(f"https://www.gravatar.com/avatar/{user.gravatar_parameter}?s=512")
+    r = get(f"https://www.gravatar.com/avatar/{user.gravatar_parameter}?s=512")
 
-        logger.info(
-            f"gravatar returned http {r.status_code} when getting avatar for user {user.name}"
-        )
+    logger.info(
+        f"gravatar returned http {r.status_code} when getting avatar for user {user.name}"
+    )
 
-        if 400 <= r.status_code <= 499:
-            # avatar not found.
-            user.get_gravatar = False
-            user.save()
-            continue
-        elif r.status_code != 200:
-            continue
+    if 400 <= r.status_code <= 499:
+        # avatar not found.
+        user.get_gravatar = False
+        user.save()
+        return
+    elif r.status_code != 200:
+        return
 
-        with NamedTemporaryFile(delete=True) as tmp_img:
-            for chunk in r:
-                tmp_img.write(chunk)
-            tmp_img.flush()
+    with NamedTemporaryFile(delete=True) as tmp_img:
+        for chunk in r:
+            tmp_img.write(chunk)
+        tmp_img.flush()
 
-            user.avatar.save(f"{user.gravatar_parameter}.jpg", File(tmp_img))
-            user.get_gravatar = False
-            user.save()
+        user.avatar.save(f"{user.gravatar_parameter}.jpg", File(tmp_img))
+        user.get_gravatar = False
+        user.save()
 
-            logger.info(f"set avatar for user {user.name} to {user.avatar.url}")
+        logger.info(f"set avatar for user {user.name} to {user.avatar.url}")
