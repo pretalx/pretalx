a00e00ecbd60671d20130d954265386a8644897f
Skyler Burger <skylerburger@gmail.com>
2024-05-15 16:23:02 -0700

    Updates variables to consts

--- src/pretalx/static/cfp/js/proposalTabTitles.js
+++ src/pretalx/static/cfp/js/proposalTabTitles.js
@@ -1,11 +1,11 @@
 // Parse and store title from the server's render since it may have been translated.
-let defaultTitleElements = document.title.split('::');
+const defaultTitleElements = document.title.split('::');
 
 if (defaultTitleElements.length !== 3) {
   console.error('Could not parse site title while adding proposal title change listener.');
 } else {
   document.getElementById('id_title').addEventListener('change', (event) => {
-    let newTitle = event.target.value;
+    const newTitle = event.target.value;
     if (newTitle === '') {
       document.title = defaultTitleElements.join(' :: ');
     } else {
