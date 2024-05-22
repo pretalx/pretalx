aa8c0aeefd2b406f5f87683226b1283c268116ec
Skyler Burger <skylerburger@gmail.com>
2024-05-22 07:00:45 -0700

    Adds title to tab when navigating back

--- src/pretalx/static/cfp/js/proposalTabTitles.js
+++ src/pretalx/static/cfp/js/proposalTabTitles.js
@@ -1,15 +1,23 @@
-// Parse and store title from the server's render since it may have been translated.
-const defaultTitleElements = document.title.split('::');
+const titleParts = document.title.split("::")
 
-if (defaultTitleElements.length !== 3) {
-  console.error('Could not parse site title while adding proposal title change listener.');
-} else {
-  document.getElementById('id_title').addEventListener('change', (event) => {
-    const newTitle = event.target.value;
-    if (newTitle === '') {
-      document.title = defaultTitleElements.join(' :: ');
-    } else {
-      document.title = `${newTitle} :: ${defaultTitleElements[1]} :: ${defaultTitleElements[2]}`;
+document.addEventListener("DOMContentLoaded", () => {
+    const titleInput = document.getElementById("id_title").value
+    if (titleInput !== "") {
+        document.title = `${titleInput} :: ${titleParts[1]} :: ${titleParts[2]}`
     }
-  });
+})
+
+if (titleParts.length !== 3) {
+    console.error(
+        "Could not parse site title while adding proposal title change listener.",
+    )
+} else {
+    document.getElementById("id_title").addEventListener("change", (event) => {
+        const newTitle = event.target.value
+        if (newTitle === "") {
+            document.title = titleParts.join(" :: ")
+        } else {
+            document.title = `${newTitle} :: ${titleParts[1]} :: ${titleParts[2]}`
+        }
+    })
 }
