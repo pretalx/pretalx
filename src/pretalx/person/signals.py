4367871ffd68ca3155c7131987fe758a46305557
Florian Moesch <florian@moesch.org>
2025-04-22 09:51:16 +0200

    add html_signal for speaker form

--- src/pretalx/person/signals.py
+++ src/pretalx/person/signals.py
@@ -1,6 +1,17 @@
 from django.dispatch import receiver
 
-from pretalx.common.signals import register_data_exporters
+from pretalx.common.signals import EventPluginSignal, register_data_exporters
+
+speaker_form_html = EventPluginSignal()
+"""
+This signal is sent out to display additional information on the speaker
+pages in the internal organiser area.
+
+As with all plugin signals, the ``sender`` keyword argument will contain the
+event. Additionally, the signal will be called with the ``request`` it is
+processing, and the ``person`` which is currently displayed.
+The receivers are expected to return HTML.
+"""
 
 
 @receiver(register_data_exporters, dispatch_uid="exporter_builtin_csv_speaker")
