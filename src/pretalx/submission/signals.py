9f26450583459581c8057d61c2f04f6fe9e2f137
Florian Moesch <florian@moesch.org>
2025-04-22 09:29:22 +0200

    add html_signal for submission form

--- src/pretalx/submission/signals.py
+++ src/pretalx/submission/signals.py
@@ -13,3 +13,14 @@ and ``user`` (which may be ``None``).
 When the submission is created or submitted from a draft state, ``old_state`` will be
 ``None``.
 """
+
+submission_form_html = EventPluginSignal()
+"""
+This signal is sent out to display additional information on the submission
+pages in the internal organiser area.
+
+As with all plugin signals, the ``sender`` keyword argument will contain the
+event. Additionally, the signal will be called with the ``request`` it is
+processing, and the ``submission`` which is currently displayed.
+The receivers are expected to return HTML.
+"""
