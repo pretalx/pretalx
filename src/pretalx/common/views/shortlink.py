629d6353bf8e5750a6f2be0897a502fedd8ead45
Florian Moesch <florian@moesch.org>
2025-09-03 16:43:32 +0200

    use orga urls if requesting user has orga access in any of the users events

--- src/pretalx/common/views/shortlink.py
+++ src/pretalx/common/views/shortlink.py
@@ -7,24 +7,26 @@ from pretalx.person.models.user import User
 from pretalx.submission.models.submission import Submission
 
 
+def has_event_access(user, event):
+    return not user.is_anonymous and user.has_perm("event.orga_access_event", event)
+
+
 @scopes_disabled()
 def shortlink_view(request, code, *args, **kwargs):
     with suppress(Submission.DoesNotExist):
-        submission = Submission.objects.get(code=code)
-        if not request.user.is_anonymous and request.user.has_perm(
-            "event.orga_access_event", submission.event
-        ):
+        submission = Submission.objects.select_related("event").get(code=code)
+        if has_event_access(request.user, submission.event):
             return HttpResponseRedirect(submission.orga_urls.base)
         return HttpResponseRedirect(submission.urls.public)
     with suppress(User.DoesNotExist):
         user = User.objects.get(code=code)
         if not request.user.is_anonymous and request.user.is_administrator:
             return HttpResponseRedirect(user.orga_urls.admin)
-        if profile := user.profiles.order_by("-created").first():
-            if not request.user.is_anonymous and request.user.has_perm(
-                "event.orga_access_event", profile.event
-            ):
+        profiles = user.profiles.select_related("event").order_by("-created")
+        for profile in profiles:
+            if has_event_access(request.user, profile.event):
                 return HttpResponseRedirect(profile.orga_urls.base)
+        if profile := profiles.first():
             if request.user == user:
                 return HttpResponseRedirect(profile.event.urls.user)
             return HttpResponseRedirect(profile.urls.public)
