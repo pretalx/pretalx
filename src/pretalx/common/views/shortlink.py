8b9d9302b3aba33d73a6dd1eb8d5c9e8b492cefa
Florian Moesch <florian@moesch.org>
2025-09-04 20:56:27 +0200

    check permissions

--- src/pretalx/common/views/shortlink.py
+++ src/pretalx/common/views/shortlink.py
@@ -7,27 +7,25 @@ from pretalx.person.models.user import User
 from pretalx.submission.models.submission import Submission
 
 
-def has_event_access(user, event):
-    return not user.is_anonymous and user.has_perm("event.orga_access_event", event)
-
-
 @scopes_disabled()
 def shortlink_view(request, code, *args, **kwargs):
     with suppress(Submission.DoesNotExist):
         submission = Submission.objects.select_related("event").get(code=code)
-        if has_event_access(request.user, submission.event):
+        if request.user.has_perm("submission.orga_list_submission", submission):
             return HttpResponseRedirect(submission.orga_urls.base)
-        return HttpResponseRedirect(submission.urls.public)
+        if request.user.has_perm("submission.view_public_submission", submission):
+            return HttpResponseRedirect(submission.urls.public)
     with suppress(User.DoesNotExist):
         user = User.objects.get(code=code)
-        if not request.user.is_anonymous and request.user.is_administrator:
+        if request.user.has_perm("person.administrator_user", user):
             return HttpResponseRedirect(user.orga_urls.admin)
         profiles = user.profiles.select_related("event").order_by("-created")
         for profile in profiles:
-            if has_event_access(request.user, profile.event):
+            if request.user.has_perm("person.orga_list_speakerprofile", profile):
                 return HttpResponseRedirect(profile.orga_urls.base)
         if profile := profiles.first():
             if request.user == user:
                 return HttpResponseRedirect(profile.event.urls.user)
-            return HttpResponseRedirect(profile.urls.public)
+            if request.user.has_perm("person.view_speakerprofile", profile):
+                return HttpResponseRedirect(profile.urls.public)
     raise Http404()
