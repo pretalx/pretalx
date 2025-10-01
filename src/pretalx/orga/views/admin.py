f2ff82f3caef1afb4bcaa38bc21b125b0997f692
Tobias Kunze <r@rixx.de>
2025-10-01 13:17:28 +0200

    Use CRUDView for admin user views

--- src/pretalx/orga/views/admin.py
+++ src/pretalx/orga/views/admin.py
@@ -8,7 +8,7 @@ from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
-from django.views.generic import DetailView, FormView, ListView, TemplateView
+from django.views.generic import FormView, TemplateView
 from django_context_decorator import context
 from django_scopes import scopes_disabled
 
@@ -18,8 +18,10 @@ from pretalx.common.image import gravatar_csp
 from pretalx.common.models.settings import GlobalSettings
 from pretalx.common.text.phrases import phrases
 from pretalx.common.update_check import check_result_table, update_check
-from pretalx.common.views.mixins import ActionConfirmMixin, PermissionRequired
+from pretalx.common.views.generic import OrgaCRUDView
+from pretalx.common.views.mixins import PermissionRequired
 from pretalx.orga.forms.admin import UpdateSettingsForm
+from pretalx.orga.tables.admin import AdminUserTable
 from pretalx.person.models import User
 
 
@@ -80,104 +82,74 @@ class UpdateCheckView(PermissionRequired, FormView):
         return reverse("orga:admin.update")
 
 
-class AdminUserList(PermissionRequired, ListView):
-    template_name = "orga/admin/user_list.html"
-    permission_required = "person.administrator_user"
+class AdminUserView(OrgaCRUDView):
     model = User
-    context_object_name = "users"
+    table_class = AdminUserTable
+    permission_required = "person.administrator_user"
     paginate_by = "250"
-
-    def dispatch(self, *args, **kwargs):
-        with scopes_disabled():
-            return super().dispatch(*args, **kwargs)
-
-    def get_queryset(self):
-        search = self.request.GET.get("q", "").strip()
-        if not search or len(search) < 3:
-            return User.objects.none()
-        return (
-            User.objects.filter(Q(name__icontains=search) | Q(email__icontains=search))
-            .prefetch_related(
-                "teams",
-                "teams__organiser",
-                "teams__organiser__events",
-                "teams__limit_events",
-            )
-            .annotate(
-                submission_count=Count("submissions", distinct=True),
-            )
-        )
-
-    def post(self, request, *args, **kwargs):
-        action = request.POST.get("action") or "-"
-        action, user_id = action.split("-")
-        user = User.objects.get(pk=user_id)
-        if action == "reset":
-            user.reset_password(event=None)
-            messages.success(request, phrases.base.password_reset_success)
-        return super().get(request, *args, **kwargs)
-
-
-class AdminUserDetail(PermissionRequired, DetailView):
-    template_name = "orga/admin/user_detail.html"
-    permission_required = "person.administrator_user"
-    model = User
-    context_object_name = "user"
-    slug_url_kwarg = "code"
-    slug_field = "code"
+    lookup_field = "code"
+    path_converter = "slug"
+    template_namespace = "orga/admin"
+    extra_actions = {"detail": {"get": "detail", "post": "reset_password"}}
+    detail_is_update = False
 
     @gravatar_csp()
     def dispatch(self, *args, **kwargs):
         with scopes_disabled():
             return super().dispatch(*args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        action = request.POST.get("action") or "-"
-        if action == "pw-reset":
-            self.get_object().reset_password(event=None)
-            messages.success(request, phrases.base.password_reset_success)
+    def get_queryset(self):
+        if self.action == "list":
+            search = self.request.GET.get("q", "").strip()
+            if not search or len(search) < 3:
+                return User.objects.none()
+            qs = User.objects.filter(
+                Q(name__icontains=search) | Q(email__icontains=search)
+            )
+        else:
+            qs = User.objects.all()
+        return qs.prefetch_related(
+            "teams",
+            "teams__organiser",
+            "teams__organiser__events",
+            "teams__limit_events",
+        ).annotate(
+            submission_count=Count("submissions", distinct=True),
+        )
+
+    def has_permission(self, *args):
+        return self.request.user.is_administrator
+
+    def reset_password(self, request, *args, **kwargs):
+        user = self.get_object()
+        user.reset_password(event=None)
+        messages.success(request, phrases.base.password_reset_success)
         return redirect(self.get_success_url())
 
     def get_success_url(self):
         return reverse("orga:admin.user.list")
 
-    @context
-    def tablist(self):
-        return {
-            "teams": _("Teams"),
-            "submissions": _("Proposals"),
-            "actions": _("Last actions"),
-        }
-
     def get_context_data(self, **kwargs):
         result = super().get_context_data(**kwargs)
-        result["teams"] = self.object.teams.all().prefetch_related(
-            "organiser", "limit_events", "organiser__events"
-        )
-        result["submissions"] = self.object.submissions.all()
-        result["last_actions"] = self.object.own_actions()[:10]
+        if self.action == "detail":
+            result["teams"] = self.object.teams.all().prefetch_related(
+                "organiser", "limit_events", "organiser__events"
+            )
+            result["submissions"] = self.object.submissions.all()
+            result["last_actions"] = self.object.own_actions()[:10]
+            result["tablist"] = {
+                "teams": _("Teams"),
+                "submissions": _("Proposals"),
+                "actions": _("Last actions"),
+            }
         return result
 
+    def get_generic_title(self, instance=None):
+        if instance:
+            return instance.name
+        return _("Users")
 
-class AdminUserDelete(ActionConfirmMixin, AdminUserDetail):
-
-    @property
-    def action_object_name(self):
-        return _("User") + f": {self.get_object().name}"
-
-    @property
-    def action_next_url(self):
-        return self.get_success_url()
-
-    @property
-    def action_back_url(self):
-        return reverse("orga:admin.user.view", kwargs={"code": self.get_object().code})
-
-    def dispatch(self, *args, **kwargs):
-        with scopes_disabled():
-            return super().dispatch(*args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
+    def delete(self, request, *args, **kwargs):
         user = self.get_object()
         try:
             user.shred()
@@ -185,9 +157,6 @@ class AdminUserDelete(ActionConfirmMixin, AdminUserDetail):
             user.deactivate()
         messages.success(request, _("The user has been deleted."))
         return redirect(self.get_success_url())
-
-    def get_success_url(self):
-        return reverse("orga:admin.user.list")
 
 
 def healthcheck(request):
