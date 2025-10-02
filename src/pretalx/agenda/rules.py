93db881ddff1525e31980ba760a8a81e424972ba
Tobias Kunze <r@rixx.de>
2025-10-02 10:33:57 +0200

    Make sure submission objects have been saved

--- src/pretalx/agenda/rules.py
+++ src/pretalx/agenda/rules.py
@@ -20,6 +20,7 @@ def is_submission_visible_via_featured(user, submission):
 def is_submission_visible_via_schedule(user, submission):
     return bool(
         submission
+        and submission.pk
         and is_agenda_visible(user, submission.event)
         and (submission.slot and submission.slot.is_visible)
     )
