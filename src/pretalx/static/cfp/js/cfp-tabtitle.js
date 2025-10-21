51e018a051e4cf134b4c4796e8c1969199cafe17
Tobias Kunze <r@rixx.de>
2025-10-21 15:12:37 +0200

    Make title update more resilient

--- src/pretalx/static/cfp/js/cfp-tabtitle.js
+++ src/pretalx/static/cfp/js/cfp-tabtitle.js
@@ -12,8 +12,8 @@ const updateTitle = (newTitle) => {
 }
 
 const checkForTitle = () => {
-    const titleInput = document.getElementById("id_title").value
-    updateTitle(titleInput)
+    const titleInput = document.getElementById("id_title")
+    if (titleInput) updateTitle(titleInput.value)
 }
 
 if (titleParts.length !== 3) {
