11a351377d20dedd733be91bdd29371d294b3370
Tobias Kunze <r@rixx.de>
2025-11-15 16:57:21 +0100

    Empty dialog on close to prevent flickering

--- src/pretalx/static/common/js/ui/history-dialog.js
+++ src/pretalx/static/common/js/ui/history-dialog.js
@@ -5,6 +5,9 @@ const setupHistoryDialog = () => {
     const dialog = document.getElementById('dialog-history-details')
     if (!dialog) return
 
+    const dialogContent = document.getElementById('dialog-history-details-content')
+    if (!dialogContent) return
+
     document.querySelectorAll('.log-detail[hx-get]').forEach((link) => {
         link.addEventListener('click', function(event) {
             event.preventDefault()
@@ -12,6 +15,8 @@ const setupHistoryDialog = () => {
             dialog.showModal()
         })
     })
+
+    dialog.addEventListener('close', () => { dialogContent.innerHTML = '' })
 }
 
 onReady(setupHistoryDialog)
