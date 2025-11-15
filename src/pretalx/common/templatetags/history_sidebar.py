75c2dd7d41a461a0936b7683edb3e05bc0ab58dd
Tobias Kunze <r@rixx.de>
2025-11-15 15:29:15 +0100

    Improve change rendering

--- src/pretalx/common/templatetags/history_sidebar.py
+++ src/pretalx/common/templatetags/history_sidebar.py
@@ -24,3 +24,50 @@ def history_sidebar(context, obj, limit=25, show_details_link=True):
         "history_class": "history-sidebar",
         "show_history_title": True,
     }
+
+
+@register.inclusion_tag("common/change_row.html", takes_context=True)
+def change_row(context, field, change):
+    event = context.get("request").event
+    rows = []
+
+    question = change.get("question")
+    label = question.question if question else change.get("label", field)
+    old_value = change.get("old")
+    new_value = change.get("new")
+    is_i18n = isinstance(old_value, dict) or isinstance(new_value, dict)
+
+    if is_i18n:
+        if not isinstance(old_value, dict):
+            old_value = {event.locale: old_value or ""}
+        if not isinstance(new_value, dict):
+            new_value = {event.locale: new_value or ""}
+
+        languages = set(old_value.keys()) | set(new_value.keys())
+        for lang in languages:
+            rows.append(
+                {
+                    "label": None,
+                    "old": old_value.get(lang),
+                    "new": new_value.get(lang),
+                    "language": lang,
+                    "question": change.get("question"),
+                }
+            )
+        rows[0]["label"] = label
+        rows[0]["rowspan"] = len(languages)
+    else:
+        rows.append(
+            {
+                "label": label,
+                "old": old_value,
+                "new": new_value,
+                "language": None,
+                "question": change.get("question"),
+            }
+        )
+
+    return {
+        "rows": rows,
+        "field": field,
+    }
