061d24ac61e9d7649d357f31b96a80a3195831fc
Tobias Kunze <r@rixx.de>
2025-12-09 21:25:25 +0100

    Start CfP editor rewrite: support reordering fields

--- src/pretalx/cfp/forms/cfp.py
+++ src/pretalx/cfp/forms/cfp.py
@@ -6,20 +6,37 @@ class CfPFormMixin:
     """All forms used in the CfP step process should use this mixin.
 
     It serves to make it work with the CfP Flow editor, e.g. by allowing
-    users to change help_text attributes of fields. Needs to go first
-    before all other forms changing help_text behaviour.
+    users to change help_text attributes of fields and reorder fields.
+    Needs to go first before all other forms changing help_text behaviour.
     """
 
     def __init__(self, *args, field_configuration=None, **kwargs):
         super().__init__(*args, **kwargs)
         self.field_configuration = field_configuration
         if self.field_configuration:
+            field_order = [field_data["key"] for field_data in self.field_configuration]
             self.field_configuration = {
                 field_data["key"]: field_data for field_data in field_configuration
             }
             for field_data in self.field_configuration:
                 if field_data in self.fields:
                     self._update_cfp_texts(field_data)
+            self._reorder_fields(field_order)
+
+    def _reorder_fields(self, field_order):
+        if not field_order:
+            return
+
+        new_fields = {}
+        for key in field_order:
+            if key in self.fields:
+                new_fields[key] = self.fields[key]
+        # Add any remaining fields not in the config
+        for key, field in self.fields.items():
+            if key not in new_fields:
+                new_fields[key] = field
+
+        self.fields = new_fields
 
     def _update_cfp_texts(self, field_name):
         field = self.fields.get(field_name)
