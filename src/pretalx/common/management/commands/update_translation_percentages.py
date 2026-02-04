192f0248d0d5be9d27e00873d4b54b770ccd3965
Tobias Kunze <r@rixx.de>
2026-02-04 19:32:18 +0100

    Fix translation percentage calculation

--- src/pretalx/common/management/commands/update_translation_percentages.py
+++ src/pretalx/common/management/commands/update_translation_percentages.py
@@ -3,50 +3,31 @@
 
 import gettext as gettext_module
 import json
+from pathlib import Path
 
+import polib
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.utils.translation import to_locale
 
 
-def get_language_score(locale):
-    catalog = {}
-    localedir = settings.LOCALE_PATHS[0]
-
-    try:
-        translation = gettext_module.translation(
-            domain="django",
-            localedir=localedir,
-            languages=[to_locale(locale)],
-            fallback=False,
-        )
-    except Exception:
-        return 1
-    catalog.update(translation._catalog.copy())
-
-    while translation._fallback:
-        if not locale.startswith(translation._fallback.info().get("language", "XX")):
-            break
-        translation = translation._fallback
-        catalog.update(translation._catalog.copy())
-
-    if not catalog:
-        return 1
-    source_strings = [k[1] if isinstance(k, tuple) else k for k in catalog.keys()]
-    return len(set(source_strings)) or 1
-
-
 def get_language_scores():
-    base_score = get_language_score("de")
+    localedir = settings.LOCALE_PATHS[0]
+    pot = polib.pofile(str(localedir / "django.pot"))
+    total = len(pot)
     percentages = {}
 
-    for lang_code in settings.LANGUAGES_INFORMATION.keys():
-        if lang_code in ("en", "de"):
+    for lang_code in settings.LANGUAGES_INFORMATION:
+        if lang_code == "en":
             percentages[lang_code] = 100
-        else:
-            lang_score = get_language_score(lang_code)
-            percentage = round(lang_score / base_score * 100)
-            percentages[lang_code] = percentage
+            continue
+        locale = to_locale(lang_code)
+        mo_path = gettext_module.find("django", localedir=localedir, languages=[locale])
+        if not mo_path:
+            percentages[lang_code] = 0
+            continue
+        po = polib.pofile(str(Path(mo_path).with_suffix(".po")))
+        percentages[lang_code] = round(len(po.translated_entries()) / total * 100)
     return percentages
 
 
@@ -54,7 +35,6 @@ class Command(BaseCommand):
     help = "Update translation percentages in the JSON file based on actual translation completeness"
 
     def handle(self, *args, **options):
-
         updated_percentages = get_language_scores()
         json_path = settings.LOCALE_PATHS[0] / "translation_percentages.json"
         with open(json_path, "w") as f:
