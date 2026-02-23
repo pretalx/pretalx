f6b670eeae629a3b4d40d041b507ab842cc1caca
Tobias Kunze <r@rixx.de>
2026-02-23 18:18:24 +0100

    Delete old existing tests

--- src/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: 2017-present Tobias Kunze
-# SPDX-License-Identifier: AGPL-3.0-only WITH LicenseRef-Pretalx-AGPL-3.0-Terms
-
-from django.test import utils
-from django_scopes import scopes_disabled
-
-utils.setup_databases = scopes_disabled()(utils.setup_databases)
