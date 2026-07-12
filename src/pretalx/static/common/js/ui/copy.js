cee44e19677ab4675be0271f708efeff9adb9b95
Tobias Kunze <r@rixx.de>
2026-07-12 07:52:44 +0200

    Fix double tooltips

--- src/pretalx/static/common/js/ui/copy.js
+++ src/pretalx/static/common/js/ui/copy.js
@@ -1,33 +1,30 @@
 // SPDX-FileCopyrightText: 2018-present Tobias Kunze
 // SPDX-License-Identifier: Apache-2.0
 
+const flashTooltip = (element, message, duration) => {
+    const wasTooltip = element.getAttribute('data-toggle') === 'tooltip'
+    const oldTooltip = element.dataset.tooltip
+    element.dataset.tooltip = message
+    if (!wasTooltip) {
+        element.setAttribute('data-toggle', 'tooltip')
+    }
+    setTimeout(() => {
+        if (oldTooltip === undefined) {
+            delete element.dataset.tooltip
+        } else {
+            element.dataset.tooltip = oldTooltip
+        }
+        if (!wasTooltip) {
+            element.removeAttribute('data-toggle')
+        }
+    }, duration)
+}
+
 const performCopy = (element) => {
     navigator.clipboard.writeText(element.dataset.destination).then(() => {
-        const wasTooltip = element.getAttribute('data-toggle') === 'tooltip'
-        const oldTitle = element.title || ''
-        element.title = element.dataset.successMessage || 'Copied!'
-        if (!wasTooltip) {
-            element.setAttribute('data-toggle', 'tooltip')
-        }
-        setTimeout(() => {
-            element.title = oldTitle
-            if (!wasTooltip) {
-                element.removeAttribute('data-toggle')
-            }
-        }, 1000)
+        flashTooltip(element, element.dataset.successMessage || 'Copied!', 1000)
     }, () => {
-        const wasTooltip = element.getAttribute('data-toggle') === 'tooltip'
-        const oldTitle = element.title || ''
-        element.title = element.dataset.errorMessage || 'Failed to copy'
-        if (!wasTooltip) {
-            element.setAttribute('data-toggle', 'tooltip')
-        }
-        setTimeout(() => {
-            element.title = oldTitle
-            if (!wasTooltip) {
-                element.removeAttribute('data-toggle')
-            }
-        }, 2000)
+        flashTooltip(element, element.dataset.errorMessage || 'Failed to copy', 2000)
     })
 }
 
