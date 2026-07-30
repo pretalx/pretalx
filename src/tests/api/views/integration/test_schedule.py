6837d01b8452f92a795b72f06b474df2287d1bfe
Tobias Kunze <r@rixx.de>
2026-07-30 16:13:15 +0200

    Improve schedule API performance

--- src/tests/api/views/integration/test_schedule.py
+++ src/tests/api/views/integration/test_schedule.py
@@ -435,7 +435,7 @@ def test_slot_list_orga_default_current_schedule(
     with scopes_disabled():
         expected_ids = set(event.current_schedule.talks.values_list("pk", flat=True))
 
-    with django_assert_num_queries(10):
+    with django_assert_num_queries(13):
         response = client.get(
             event.api_urls.slots,
             follow=True,
@@ -842,7 +842,7 @@ def test_slot_list_query_count(client, event, item_count, django_assert_num_quer
         with scope(event=event):
             freeze_schedule(event.wip_schedule, "v1", notify_speakers=False)
 
-    with django_assert_num_queries(5):
+    with django_assert_num_queries(8):
         response = client.get(event.api_urls.slots, follow=True)
 
     assert response.status_code == 200
