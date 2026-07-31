b938b872f3ae77b301b1e315aeb28d24e4e56666
Tobias Kunze <r@rixx.de>
2026-07-31 09:52:57 +0200

    Improve changelog performance

--- src/pretalx/schedule/domain/changelog.py
+++ src/pretalx/schedule/domain/changelog.py
@@ -4,7 +4,8 @@
 from collections import defaultdict
 
 from pretalx.schedule.domain.queries.schedule import published_schedules
-from pretalx.schedule.models import TalkSlot
+from pretalx.schedule.models import Room, TalkSlot
+from pretalx.submission.models import Submission
 
 
 def build_changelog(event):
@@ -20,7 +21,7 @@ def build_changelog(event):
             schedules[i + 1] if i + 1 < len(schedules) else None
         )
 
-    slots = (
+    slots = list(
         TalkSlot.objects.filter(
             schedule_id__in=[s.pk for s in schedules],
             room__isnull=False,
@@ -28,11 +29,30 @@ def build_changelog(event):
             is_visible=True,
             submission__isnull=False,
         )
-        .select_related("submission", "submission__event", "room")
-        .with_sorted_speakers()
     )
+
+    # We populate rooms and submissions manually, as otherwise the query will
+    # return each room and each submission roughly once per version, resulting
+    # in tens of thousands of wide rows for events with many releases.
+    rooms = {
+        room.pk: room
+        for room in Room.objects.filter(pk__in={slot.room_id for slot in slots})
+    }
+    submissions = {
+        submission.pk: submission
+        for submission in Submission.all_objects.filter(
+            pk__in={slot.submission_id for slot in slots}
+        ).with_sorted_speakers()
+    }
+    for submission in submissions.values():
+        submission.event = event
+    schedules_by_id = {schedule.pk: schedule for schedule in schedules}
+
     slots_by_schedule = defaultdict(list)
     for slot in slots:
+        slot.room = rooms[slot.room_id]
+        slot.submission = submissions[slot.submission_id]
+        slot.schedule = schedules_by_id[slot.schedule_id]
         slots_by_schedule[slot.schedule_id].append(slot)
     for schedule in schedules:
         schedule.__dict__["scheduled_talks"] = slots_by_schedule.get(schedule.pk, [])
