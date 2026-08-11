ef715c66c0146f8fde50c79fc067753c13e908cf
Tobias Kunze <r@rixx.de>
2026-08-11 15:46:58 +0200

    Implement alert component

--- src/pretalx/common/templatetags/components.py
+++ src/pretalx/common/templatetags/components.py
@@ -2,4 +2,8 @@
 # SPDX-License-Identifier: AGPL-3.0-only WITH LicenseRef-Pretalx-AGPL-3.0-Terms
 from pretalx.common.components import component, register
 
+component(
+    "alert", "common/ui/alert.html", props=("id", "level"), defaults={"level": "info"}
+)
+
 __all__ = ["component", "register"]
