582126e2c55b05c83ea2db4c5c5d42aa310825f1
Tobias Kunze <r@rixx.de>
2026-09-03 15:00:56 +0200

    Fix filesizewarning in dynamic fields

--- src/pretalx/static/common/js/forms/filesize.js
+++ src/pretalx/static/common/js/forms/filesize.js
@@ -2,37 +2,32 @@
 // SPDX-License-Identifier: Apache-2.0
 
 const warnFileSize = (element) => {
+    unwarnFileSize(element)
     const warning = document.createElement("div")
     warning.classList = ["invalid-feedback"]
     warning.textContent = element.dataset.sizewarning
     element.parentElement.appendChild(warning)
     element.setAttribute("aria-invalid", "true")
+    element.setCustomValidity(element.dataset.sizewarning)
 }
 const unwarnFileSize = (element) => {
     element.removeAttribute("aria-invalid")
+    element.setCustomValidity("")
     const warning = element.parentElement.querySelector(".invalid-feedback")
     if (warning) element.parentElement.removeChild(warning)
 }
 
-const initFileSizeCheck = (element) => {
-    const checkFileSize = () => {
-        const files = element.files
-        if (!files || !files.length) {
-            unwarnFileSize(element)
-        } else {
-            maxsize = parseInt(element.dataset.maxsize)
-            if (files[0].size > maxsize) {
-                warnFileSize(element)
-            } else {
-                unwarnFileSize(element)
-            }
-        }
+const checkFileSize = (element) => {
+    const files = element.files
+    const maxsize = parseInt(element.dataset.maxsize)
+    if (files && files.length && files[0].size > maxsize) {
+        warnFileSize(element)
+    } else {
+        unwarnFileSize(element)
     }
-    element.addEventListener("change", checkFileSize, false)
 }
 
-onReady(() => {
-    document
-        .querySelectorAll("input[data-maxsize][type=file]")
-        .forEach((element) => initFileSizeCheck(element))
+document.addEventListener("change", (event) => {
+    const element = event.target
+    if (element.matches("input[data-maxsize][type=file]")) checkFileSize(element)
 })
